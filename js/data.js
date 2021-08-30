3cac9e330f4ea08ddd1da27bd590781e57f67d6b
aarjitpaudel <74756923+aarjitpaudel@users.noreply.github.com>
2021-08-30 12:35:02 +0545

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,31 @@
+var data = {
+    name: [
+        { name: 'Name', value: 'Aarjit Paudel' }
+    ],
+
+    bio: [
+        { name: 'Name', value: 'Aarjit Paudel' },
+        { name: 'Email', value: 'aarjitpaudel64@gmail.com' },
+        { name: 'Country', value: 'Nepal' },
+        { name: 'City', value: 'Pokhara' }
+    ],
+    github: [
+        { name: 'GitHub', value: 'aarjitpaudel' }
+    ],
+    projects: [
+        { name: 'name', value: 'value' },
+        { name: 'name', value: 'value' },
+        { name: 'name', value: 'value' },
+        { name: 'name', value: 'value' },
+        { name: 'name', value: 'value' },
+        { name: 'name', value: 'value' },
+        { name: 'name', value: 'value' }
+    ],
+    resume: 'value',
+    socials: [
+        { name: 'socail', value: 'link' },
+        { name: 'social', value: 'link' },
+        { name: 'social', value: 'link' },
+        { name: 'social', value: 'link' }
+    ]
+}
